04a66aa4c76512741915fcf79eb6798808f4fc91
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-01 23:01:18 -0400

    start button works!

--- gameschematics.js
+++ gameschematics.js
@@ -3,15 +3,19 @@ var startbutton = document.getElementById("startbutton");
 
 
 function quizTime() {
-var startscreen = getElementById("startscreen");
-startscreen.setAttribute("class", "hide");
+var startscreen = document.getElementById("startscreen");
+startscreen.setAttribute("id", "hide");
 
+quizTimeStart()
+}
+
+function quizTimeStart() {
+var startquiz = document.getElementById(startquiz)
+startquiz.removeAttribute("id", "hide");
 }
 
 
 
-
-
-startbutton.onclick = quizTime();
+startbutton.onclick = quizTime;
 
 
