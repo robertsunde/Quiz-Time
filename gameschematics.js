85e4c35286cf3b04c8f4ddf219b7d35677a6dd88
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-02 02:07:30 -0400

    troubleshooting quiz display issues

--- gameschematics.js
+++ gameschematics.js
@@ -1,5 +1,5 @@
 // var firstQuestion = questions[0];
-
+let timeRemaining = 60
 var questions = [
 
     { 
@@ -54,35 +54,34 @@ function startQuestions() {
 
 var startquiz = document.getElementById("hiddenquestions")
 startquiz.removeAttribute("id");
-var questionText = document.getElementById("questionText");
-questionText.textContent = questions[0].title;
+// var questionText = document.getElementById("questionText");
+// questionText.textContent = questions[0].title;
 
-var option1Text = document.getElementById("option1");
-option1Text.textContent = questions[0].options[0];
+// var option1Text = document.getElementById("option1");
+// option1Text.textContent = questions[0].options[0];
 
-var option2Text = document.getElementById("option2");
-option2Text.textContent = questions[0].options[1];
+// var option2Text = document.getElementById("option2");
+// option2Text.textContent = questions[0].options[1];
 
-var option3Text = document.getElementById("option3");
-option3Text.textContent = questions[0].options[2];
+// var option3Text = document.getElementById("option3");
+// option3Text.textContent = questions[0].options[2];
 
-var option4Text = document.getElementById("option4");
-option4Text.textContent = questions[0].options[3];
-
-
+// var option4Text = document.getElementById("option4");
+// option4Text.textContent = questions[0].options[3];
 
 
-questionone()
+
+
+// questionone()
 startTimer()
 }
 
-function secondQuestion() {console.log("win")}
-function wrong() {console.log("loser")}
+
 
 
 function startTimer() {
 let timer = document.getElementById("timer")
-let timeRemaining = 60
+
 let timerstart = setInterval(function() {
 timeRemaining--;
 timer.textContent = timeRemaining;
@@ -97,7 +96,14 @@ if(timeRemaining === 0) {
 }
 
 
+function rightone() {
+    console.log("win")
+}
 
+function wrongone() {
+    console.log("lose")
+    timeRemaining -= 7
+}
 
 function gameOver() {
     console.log("game over")
