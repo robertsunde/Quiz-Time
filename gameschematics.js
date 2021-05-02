f2d85fa63e8b48ac70406ce2a42204a022a2a8f8
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-01 22:02:15 -0400

    troubleshooting start button hide attribute

---
+++ gameschematics.js
@@ -0,0 +1,17 @@
+var startbutton = document.getElementById("startbutton");
+
+
+
+function quizTime() {
+var startscreen = getElementById("startscreen");
+startscreen.setAttribute("class", "hide");
+
+}
+
+
+
+
+
+startbutton.onclick = quizTime();
+
+
